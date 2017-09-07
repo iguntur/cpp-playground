7b20785705544c727a9583f68d6bd876479482a2
Guntur Poetra <poetra.guntur@gmail.com>
2017-09-08 03:01:03 +0700

    add array.cpp file

---
+++ src/basic/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int arr[3] = {10, 11, 20};
+
+	std::cout << arr[2] << std::endl;
+	std::cout << arr[1] << std::endl;
+	std::cout << arr[0] << std::endl;
+
+	return 0;
+}
