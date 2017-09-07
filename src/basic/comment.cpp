845ea1e55107586c049be12c5bef28bff5012712
Guntur Poetra <poetra.guntur@gmail.com>
2017-09-08 03:02:11 +0700

    add comment.cpp file. How to use comment?

---
+++ src/basic/comment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/**
+ * Main program
+ *
+ * Write multiple lines comment
+ */
+
+int main()
+{
+	// In the body function, it is possible to write your comment.
+	std::cout << "write comment start with //" << std::endl;
+	std::cout << "or block with /* ... */" << std::endl;
+
+	return 0;
+}
