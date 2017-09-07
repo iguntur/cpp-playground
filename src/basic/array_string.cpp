e848f0cb3b7db79969990e71e5e105c30f89c36b
Guntur Poetra <poetra.guntur@gmail.com>
2017-09-08 03:01:12 +0700

    add array_string.cpp file

---
+++ src/basic/array_string.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <string>
+
+int main()
+{
+	std::vector<std::string> instruments;
+
+	instruments.push_back("guitar");
+	instruments.push_back("bass");
+	instruments.push_back("keyboard");
+	instruments.push_back("violin");
+	instruments.push_back("saxophone");
+
+	return 0;
+}
